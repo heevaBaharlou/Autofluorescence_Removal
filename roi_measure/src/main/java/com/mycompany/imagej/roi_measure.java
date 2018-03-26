d0de59751200dae142afb1e727897bc044a04e3c
Nicolas Canete <ncan4475@uni.sydney.edu.au>
2018-03-26 14:15:35 +1100

    Dilate Autofluorescent ROIs

--- roi_measure/src/main/java/com/mycompany/imagej/roi_measure.java
+++ roi_measure/src/main/java/com/mycompany/imagej/roi_measure.java
@@ -6,6 +6,7 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.Roi;
+import ij.plugin.ChannelSplitter;
 import ij.plugin.PlugIn;
 import ij.plugin.frame.RoiManager;
 import ij.process.ImageProcessor;
@@ -17,23 +18,31 @@ public class roi_measure implements PlugIn {
     public void run(String arg) {
 
         /* Load in image */
+        // May need to allow user to input which image to run threshold on
+        // Indices start at 1, not 0 for these values
+        int indexToThreshold = 1;
+        int indexSecondImage = 2;
+        int numberDilations = 5;
+        double thresholdValue = 0.6;
+
         IJ.log("Thresholding...");
         ImagePlus imp = IJ.getImage();
         ImageStack is = imp.getStack();
-        ImageProcessor ip = is.getProcessor(1);
-        ImageProcessor ip2 = is.getProcessor(2);
+        ImageProcessor ip1 = is.getProcessor(indexToThreshold);
+        ImageProcessor ip2 = is.getProcessor(indexSecondImage);
 
         /* Threshold to get ROIs */
-        // May need to allow user input here
-        ImagePlus toThreshold = imp.duplicate();
+        ImagePlus duplicate = imp.duplicate();
+        ImagePlus[] imageSplit = ChannelSplitter.split(duplicate);
+        ImagePlus toThreshold = imageSplit[indexToThreshold];
         IJ.run(toThreshold, "Auto Local Threshold", "method=Niblack radius=15 parameter_1=0 parameter_2=0 white");
         IJ.run(toThreshold, "Analyze Particles...", "size=100-10000 pixel show=Nothing clear add slice");
         toThreshold.close();
 
         /* Get ROIs from ROI manager */
         IJ.log("Getting ROIs...");
-        RoiManager manager = RoiManager.getInstance();
-        int[] roiIndex = manager.getIndexes(); // Break if length = 0 or null?
+        RoiManager rm = RoiManager.getInstance();
+        int[] roiIndex = rm.getIndexes(); // Break if length = 0 or null?
 
         /* Get ROI information, masks and bounding rectangles */
         // Arrays of masks, ROIs, and bounding rectangles
@@ -42,71 +51,64 @@ public class roi_measure implements PlugIn {
         Rectangle[] boundingRectangles = new Rectangle[roiIndex.length];
         int[] sizes = new int[roiIndex.length];
 
-        int maxSize = 0;
-
         for (int i = 0; i < roiIndex.length; i++) {
             // Get masks, ROIs, and bounding rectangles
-            rois[i] = manager.getRoi(roiIndex[i]);
+            rois[i] = rm.getRoi(roiIndex[i]);
             masks[i] = rois[i].getMask();
             boundingRectangles[i] = rois[i].getBounds();
-
-            // Get size of largest bounding rectangle
-            // Should be a more efficient way of doing this?
             sizes[i] = boundingRectangles[i].height * boundingRectangles[i].width;
-            if (sizes[i] > maxSize) {
-                maxSize = sizes[i];
-            }
         }
 
-        /* Store ROI pixel intensities into a matrix - each ROI represented by a row */
+        /* Store Correlation Coefficients of ROIs */
         IJ.log("Measuring Correlation Coefficients...");
         double[] correlationCoefficients = new double[roiIndex.length];
 
         for (int i = 0; i < roiIndex.length; i++) {
-            double[] pixelIntensity = new double[sizes[i]];
+            double[] pixelIntensity1 = new double[sizes[i]];
             double[] pixelIntensity2 = new double[sizes[i]];
             int j = 0;
             for (int y = 0; y < boundingRectangles[i].height; y++) {
                 for (int x = 0; x < boundingRectangles[i].width; x++) {
                     if (masks[i] == null || masks[i].getPixel(x,y) != 0) {
-                        pixelIntensity[j] = ip.getPixelValue(boundingRectangles[i].x + x,
-                                                             boundingRectangles[i].y + y);
-                        pixelIntensity2[j] = ip2.getPixelValue(boundingRectangles[i].x + x,
-                                                              boundingRectangles[i].y + y);
+                        pixelIntensity1[j] = ip1.getPixelValue(boundingRectangles[i].x + x, boundingRectangles[i].y + y);
+                        pixelIntensity2[j] = ip2.getPixelValue(boundingRectangles[i].x + x, boundingRectangles[i].y + y);
                     } else {
-                        pixelIntensity[j] = -1;
+                        pixelIntensity1[j] = -1;
                         pixelIntensity2[j] = -1;
                     }
                     j += 1;
                 }
             }
-            correlationCoefficients[i] = correlationCoefficient(pixelIntensity, pixelIntensity2);
-            IJ.log(String.valueOf(correlationCoefficients[i]));
+            correlationCoefficients[i] = correlationCoefficient(pixelIntensity1, pixelIntensity2);
+            // IJ.log(String.valueOf(correlationCoefficients[i]));
         }
-//        double[][] pixelIntensity = new double[roiIndex.length][maxSize];
-//
-//        // Loop through ROIs
-//        for (int i = 0; i < roiIndex.length; i++) {
-//            // IJ.log("Doing ROI:");
-//            // IJ.log(String.valueOf(i));
-//            int count = 0;
-//            for (int y = 0; y < boundingRectangles[i].height; y++) {
-//                for (int x = 0; x < boundingRectangles[i].width; x++) {
-//                    if (masks[i] == null || masks[i].getPixel(x, y) != 0) {
-//                        pixelIntensity[i][count] = ip.getPixelValue(boundingRectangles[i].x + x,
-//                                boundingRectangles[i].y + y);
-//                        // IJ.log(String.valueOf(pixelIntensity[i][count]));
-//                    } else {
-//                        pixelIntensity[i][count] = -1; // -1 if outside of ROI
-//                    }
-//                    count = count + 1;
-//                }
-//            }
-//            // Set all extra row entries to -1
-//            for (int j = count; j < maxSize; j++) {
-//                pixelIntensity[i][j] = -1; // Might be a better way to do this?
-//            }
-//        }
+
+        /* Dilate Autofluorescent ROI */
+        IJ.log("Dilating ROIs...");
+
+        // Count number of Autofluorescent ROIs
+        // Might not need this - inefficient
+        int autofluorescentRoiCount = 0;
+        for (int i = 0; i < roiIndex.length; i++) {
+            if (correlationCoefficients[i] > thresholdValue) {
+                autofluorescentRoiCount += 1;
+            }
+        }
+
+        // Get Autofluorescent ROIs and dilate them
+        ImageProcessor[] dilatedMasks = new ImageProcessor[autofluorescentRoiCount];
+        int j = 0;
+        for (int i = 0; i < roiIndex.length; i++) {
+            if (correlationCoefficients[i] > thresholdValue) {
+                dilatedMasks[j] = masks[i];
+
+                for (int k=0; k < numberDilations; k++) {
+                    dilatedMasks[j].dilate();
+                }
+                j += 1;
+            }
+        }
+
         IJ.log("Done!");
     }
 
@@ -125,6 +127,7 @@ public class roi_measure implements PlugIn {
         if (pixelIntensity1.length != pixelIntensity2.length) {
             return -1; // Should not occur, but added anyway
         } else {
+            // Could easily make this more efficient
             double roiMean1 = roiMean(pixelIntensity1);
             double roiMean2 = roiMean(pixelIntensity2);
             double[][] coefficientVals = new double[2][pixelIntensity1.length];
@@ -143,7 +146,6 @@ public class roi_measure implements PlugIn {
                 d2 += coefficientVals[1][i] * coefficientVals[1][i];
             }
             return n1/(Math.sqrt(d1)*Math.sqrt(d2));
-
         }
     }
 }
